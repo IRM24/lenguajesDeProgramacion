90659d49d91fc53fce6ddcf0568697917c92aad9
IRM24 <ianmusic2404@gmail.com>
2023-09-28 21:37:48 -0600

    practica prolog

---
+++ Practica Prolog #1/example2.pl
@@ -0,0 +1,57 @@
+:- use_module(library(strings)).
+
+sumlist([], 0).     % La suma de una lista vac�a es 0.
+sumlist([X|Resto], S) :-
+    sumlist(Resto, SResto),  % Suma de los elementos del resto de la lista.
+    S is X + SResto.        % S es la suma del primer elemento y SResto.
+
+
+subconj(_,[]).
+
+
+subconj(S,[H|T]) :-
+    member(H, S),
+    subconj(S, T).
+
+
+
+aplanar([], []).
+
+
+aplanar([H|T], L2) :-
+    is_list(H),
+    aplanar(H, H1),
+    aplanar(T, T1),
+    append(H1, T1, L2).
+
+
+aplanar([H|T], [H|T1]) :-
+    \+ is_list(H),
+    aplanar(T, T1).
+
+
+
+partir([], _, [], []).
+
+
+partir([H|T], Umbral, [H|Menores], Mayores) :-
+    H < Umbral,
+    partir(T, Umbral, Menores, Mayores).
+
+
+partir([H|T], Umbral, Menores, [H|Mayores]) :-
+    H >= Umbral,
+    partir(T, Umbral, Menores, Mayores).
+
+
+sub_cadenas(_, [], []).
+
+
+sub_cadenas(Sub, [H|T], [H|Filtradas]) :-
+    sub_string(H, _, _, _, Sub),
+    sub_cadenas(Sub, T, Filtradas).
+
+
+sub_cadenas(Sub, [H|T], Filtradas) :-
+    \+ sub_string(H, _, _, _, Sub),
+    sub_cadenas(Sub, T, Filtradas).
